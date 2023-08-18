2898dff5c0811f756bc16f0e5e9d7550d90ce283
phuoc <phuocdongxoai@gmail.com>
2023-08-18 14:50:01 +0700

    restart stream

--- main.go
+++ main.go
@@ -18,19 +18,25 @@ func main() {
 	}
 	stopSig := make(chan os.Signal, 1)
 	signal.Notify(stopSig, syscall.SIGINT, syscall.SIGTERM)
-	done := false
-	for !done {
+	for {
 		select {
 		case msg := <-msgs:
 			fmt.Println("message:", msg)
 		case err = <-errs:
 			fmt.Println("error when streaming:", err)
 			twitterStream.Close()
-			done = true
-		case <-stopSig:
+			<-stop // waiting for cleaning up
+
+			// restart stream when error happens like connection closed by server
+			twitterStream = stream.NewTwitterStream()
+			msgs, errs, stop, err = twitterStream.Stream()
+			if err != nil {
+				log.Fatalf("unable to start stream: %s", err.Error())
+			}
+		case <-stopSig: // user force program to stop
 			twitterStream.Close()
-			done = true
+			<-stop
+			return
 		}
 	}
-	<-stop
 }
